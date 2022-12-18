044baf96964026f3501b3f28c1b9209252ea9db7
hkrain <hkrain@uiowa.edu>
2022-12-18 01:47:04 -0600

    Started Writing Generator Class

---
+++ Generator.java
@@ -0,0 +1,12 @@
+#password generator
+import java.util.Objects;
+import java.util.Scanner;
+
+public class Generator{
+    Alphabet alphabet;
+    public static Scanner keyboard;
+
+    public Generator(Scanner scanner){
+        keyboard = scanner;
+    }
+}
