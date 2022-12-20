7a34788b5763c5e25258790b34fd1e215e2a5754
hkrain <hkrain@uiowa.edu>
2022-12-20 00:34:51 -0600

    Wrote junit tests (needs lots of work) Better titles More in depth Test outer boundaries/extreme cases More Tests Need differentiation in test (not just assertEquals)

---
+++ PasswordGenerator/src/GeneratorTest.java
@@ -0,0 +1,33 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class GeneratorTest {
+    private final Password password = new Password("Secret");
+    private final Alphabet firstAlphabet = new Alphabet(true,false,false,false);
+    private final Alphabet secondAlphabet = new Alphabet(false,true,true,true);
+    private final Generator generator = new Generator (true, false, false, false);
+
+    @Test
+    public void test1() {
+        assertEquals("Secret", password.toString());
+    }
+    @Test
+    public void test2(){
+        assertEquals(firstAlphabet.getAlphabet(), Alphabet.UPPERCASE_LETTERS);
+    }
+
+    @Test
+    public void test3(){
+        assertEquals(secondAlphabet.getAlphabet(), Alphabet.LOWERCASE_LETTERS + Alphabet.NUMBERS + Alphabet.SYMBOLS);
+    }
+
+    @Test
+    public void test4(){
+        assertEquals(generator.alphabet.getAlphabet(), Alphabet.UPPERCASE_LETTERS);
+    }
+
+    @Test
+    public void test5(){
+        assertEquals(generator.alphabet.getAlphabet().length(),26);
+    }
+}
