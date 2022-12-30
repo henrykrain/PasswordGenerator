5847b69c19de622ed458e728bd3afc3fbf1b1461
hkrain <hkrain@uiowa.edu>
2022-12-29 21:30:12 -0600

    Renamed/Refactor Karzour Tests Cases 1.)I placed the attributes inside the test cases 2.)I gave the test cases long descriptive names

--- PasswordGenerator/src/GeneratorTest.java
+++ PasswordGenerator/src/GeneratorTest.java
@@ -2,32 +2,34 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class GeneratorTest {
-    private final Password password = new Password("Secret");
-    private final Alphabet firstAlphabet = new Alphabet(true,false,false,false);
-    private final Alphabet secondAlphabet = new Alphabet(false,true,true,true);
-    private final Generator generator = new Generator (true, false, false, false);
+    //attributes like these should be inside the tests
 
-    @Test
-    public void test1() {
+    @Test //KZarzour original test, made some minor refactorations, but its still the same concept
+    public void test_does_the_method_toString_return_the_correct_value() {
+        Password password = new Password("Secret");
         assertEquals("Secret", password.toString());
     }
     @Test
-    public void test2(){
+    public void test_are_the_characters_being_used_only_caps(){
+        Alphabet firstAlphabet = new Alphabet(true,false,false,false);
         assertEquals(firstAlphabet.getAlphabet(), Alphabet.UPPERCASE_LETTERS);
     }
 
     @Test
-    public void test3(){
+    public void test_are_the_characters_being_used_everything_but_caps(){
+        Alphabet secondAlphabet = new Alphabet(false,true,true,true);
         assertEquals(secondAlphabet.getAlphabet(), Alphabet.LOWERCASE_LETTERS + Alphabet.NUMBERS + Alphabet.SYMBOLS);
     }
 
     @Test
-    public void test4(){
+    public void test_is_the_generator_creating_an_alphabet_with_only_caps(){
+        Generator generator = new Generator (true, false, false, false);
         assertEquals(generator.alphabet.getAlphabet(), Alphabet.UPPERCASE_LETTERS);
     }
 
     @Test
-    public void test5(){
+    public void test_is_the_generator_creating_an_alphabet_of_length_26_with_only_caps(){
+        Generator generator = new Generator (true, false, false, false);
         assertEquals(generator.alphabet.getAlphabet().length(),26);
     }
 }
